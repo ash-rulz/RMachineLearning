958c28590013c36be68f888f12ebfcf0db171284
aswma317 <aswma317@student.liu.se>
2023-01-18 19:50:57 +0100

    Logistic new problem

--- R/LogisticRegression.R
+++ R/LogisticRegression.R
@@ -1,3 +1,4 @@
+####Excercise problem####
 set.seed(12345)
 library(ggplot2)
 data <- read.csv(file = './data/pima-indians-diabetes.csv', header = F)
@@ -33,7 +34,7 @@ plot <- ggplot(data, aes(x = V2, y = V8)) +
 #The model is bad, as there is high rate of diabetic patients miss classified
 #as healthy. 
 
-#Formula for decision matrix
+#Formula for decision matrix. 1/2 = 1/(1+e^-z), z = theta^t * x
 dec_bound <- as.data.frame(matrix(nrow=0, ncol = 2))
 for (i in 50:200) {
   x <- i
@@ -42,11 +43,7 @@ for (i in 50:200) {
   dec_bound <- rbind(dec_bound,
                      data.frame(x=x, y= y))
 }
-# intercept <- -(mod$coefficients[1]/mod$coefficients[3])
-# slope <- -(mod$coefficients[2]/mod$coefficients[3])
 plot + geom_line(data = dec_bound, aes(x=x,  y= y))
-# + geom_abline(slope = slope, intercept = intercept)
-# + geom_line(data = dec_bound, aes(x=x,  y= y))
 #The decision boundary seems to capture all the predicted diabetic patients
 #well, but some of the non-diabetic patients are wrongly classified as diabetic
 
@@ -78,6 +75,7 @@ print(conf_mat_0.8)
 print(missclassError(conf_mat_0.8))
 #In this case, more diabetic patients are wrongly classified. This is the worst model.
 
+#Basis function expansion trick
 data$z1 <- data$V2^4
 data$z2 <- data$V2^3*data$V8
 data$z3 <- data$V2^2*data$V8^2
@@ -97,3 +95,44 @@ conf_mat_basis <- table(data$V9, data$Pred_Basis)
 print(conf_mat_basis)
 print(missclassError(conf_mat_basis))
 #The accuracy has improved a bit
+
+####https://www.projectpro.io/article/example-on-how-to-do-logistic-regression-in-r/542####
+train <- read.csv('./Data/TitanicTrain.csv', header = TRUE)
+#Data pre-processing
+data <- subset(train, select = c(2,3,5,6,7,8,10,12))
+data <- data[!is.na(data$Embarked), ]
+data$Age[is.na(data$Age)] <- round(mean(data$Age, na.rm = T), 0)
+head(data)
+data$Sex <- as.factor(data$Sex)
+data$Embarked <- as.factor(data$Embarked)
+
+#Split the data to train and test
+n <- dim(data)[1]
+set.seed(12345)
+id <- sample(1:n, floor(n*0.7))
+train <- data[id,]
+test <- data[-id,]
+
+mod <- glm(as.factor(Survived) ~ .,
+           data = train,
+           family = 'binomial')
+summary(mod)
+
+#Evaluate the performance on test data
+prob <- predict(mod, newdata = test[,2:8], type = 'response')
+pred <- ifelse(prob > 0.5, 1, 0)
+test_conf_mat <- table(test$Survived, pred)
+test_err <- missclassError(test_conf_mat)
+
+#ROC and AUC curve
+library(ROCR)
+prob <- predict(mod, newdata = test[,2:8], type = 'response')
+pred <- prediction(prob, test$Survived)
+roc_curve <- performance(pred, measure = 'tpr', x.measure = 'fpr')
+plot(roc_curve)
+
+auc <- performance(pred, measure = 'auc')
+auc <- auc@y.values[[1]]
+print(auc)#Gives AUC score, ideal score = 1
+
+
